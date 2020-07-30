029c5286b068a369f8dc7b8564a9d13894b587ac
amelie.chardon@laplateforme.io <xkktRc8q>
2020-07-30 23:06:44 +0200

    v2

--- script.js
+++ script.js
@@ -24,12 +24,10 @@ $(function() {
 });
 
 $(document).scroll(function(){
-  console.log(scrollY);
-
-if (scrollY < 671) { $("a[href]").removeClass("en-cours"); $("a[href$='a-propos']").addClass("en-cours");}
-if(671 < scrollY && scrollY <=1200){ $("a[href]").removeClass("en-cours"); $("a[href$='experiences']").addClass("en-cours");}
-if(1200< scrollY && scrollY <=2000){ $("a[href]").removeClass("en-cours"); $("a[href$='formations']").addClass("en-cours");}
-if(2000 < scrollY && scrollY <=2708){ $("a[href]").removeClass("en-cours"); $("a[href$='loisirs']").addClass("en-cours");}
-if(2708 < scrollY){ $("a[href]").removeClass("en-cours"); $("a[href$='contact']").addClass("en-cours");}
-
+    if (scrollY < 671) { $("a[href]").removeClass("en-cours"); $("a[href$='a-propos']").addClass("en-cours");}
+    if(671 < scrollY && scrollY <=1200){ $("a[href]").removeClass("en-cours"); $("a[href$='experiences']").addClass("en-cours");}
+    if(1200< scrollY && scrollY <=2100){ $("a[href]").removeClass("en-cours"); $("a[href$='formations']").addClass("en-cours");}
+    if(2100 < scrollY && scrollY <=2900){ $("a[href]").removeClass("en-cours"); $("a[href$='realisations']").addClass("en-cours");}
+    if(2900 < scrollY && scrollY <=3400){ $("a[href]").removeClass("en-cours"); $("a[href$='loisirs']").addClass("en-cours");}
+    if(3400 < scrollY){ $("a[href]").removeClass("en-cours"); $("a[href$='contact']").addClass("en-cours");}
 });
